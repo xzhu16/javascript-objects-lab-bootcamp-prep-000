8d8e86bb2fac5b6be3b4b88740eb556c5d9cc5d1
Xi Zhu <zxjucy2012@gmail.com>
2019-04-28 19:57:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-updateObjectWithKeyAndValue(obj, 'prop2', 2)
+updateObjectWithKeyAndValue(obj, 'prop2', 2);
 updateObjectWithKeyAndValue(obj, 'prop2', undefined)
 
