b225222f0ed2d8f1ba1655dc225bb1d5fde7f15b
Xi Zhu <zxjucy2012@gmail.com>
2019-04-28 20:18:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return 
+  object(key) = value;
+  return object
 }
