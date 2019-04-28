ac1ff2f5b982bc0c6fc99b5d6bffb911fb6e30ab
Xi Zhu <zxjucy2012@gmail.com>
2019-04-28 19:45:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var recipes = { };
 updateObjectWithKeyAndValue(object, key, value)
   object[key] = value;
-  return object
-  var obj = { prop:1 }
+  return object;
+  var obj = { prop:1 };
   updateObjectWithKeyAndValue(obj, 'prop2', 2)
   return obj
 
